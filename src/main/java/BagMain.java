87dc3d3ec92a1ea45d01d8c960df47a547397f80
ethanhonest <ethan.honest@mail.utoronto.ca>
2022-09-24 12:06:49 -0400

    BagMain started

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -15,7 +15,12 @@ class BagMain {
      * on new Bag types (and HandBag subclasses)!
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
-        // TODO: Implement this.
+        for (Bag b : bags) {
+            b.enhance();
+            if (double_enhance_handbags && b instanceof HandBag) {
+                b.enhance();
+            }
+        }
     }
 
     /**
@@ -28,6 +33,12 @@ class BagMain {
      * @return The total number of straps of CrossbodyBags.
      */
     public static int countCrossbodyStraps(Bag[] bags) {
-        // TODO: Implement this.
+        int strapCount = 0;
+        for (Bag b : bags) {
+            if (b instanceof CrossbodyBag) {
+                strapCount += ((CrossbodyBag) b).getNumberOfStraps();
+            }
+        }
+        return strapCount;
     }
 }
